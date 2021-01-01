71f70f412aa02cfdc228552c024ae813e08be85e
Dora F <doradora.prog@gmail.com>
2021-01-02 07:16:27 +0900

    Commit from centos

---
+++ singmarkov.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+import MeCab
+import re
+import random
+
+
+class Word:
+    def __init__(self, deep, maxi):
+        self.deep = deep
+        self.count = 0
+        self.nexts = {}
+        self.MAXI_CHAIN = maxi
+
+    def add(self, words):
+        self.count += 1
+        if len(words) < 1:
+            return
+        if len(words) < self.MAXI_CHAIN - self.deep - 1:
+            return
+        w = words.pop(0)
+        if self.deep < self.MAXI_CHAIN - 1:
+            if w not in self.nexts:
+                self.nexts[w] = Word(self.deep+1, self.MAXI_CHAIN)
+            self.nexts[w].add(words)
+
+    def show(self):
+        text = ""
+        for w in self.nexts:
+            blank = ""
+            for _ in range(self.deep):
+                blank += "  "
+            text += blank + w + "[" + str(self.nexts[w].deep) + \
+                " / " + str(self.nexts[w].count) + "]"
+            if self.deep < self.MAXI_CHAIN - 1:
+                text += " -> " + self.nexts[w].show() + "\n"
+        return text
+
+    def pick(self):
+        r = random.randrange(self.count)
+        cnt = 0
+        for k, n in self.nexts.items():
+            cnt += n.count
+            # print(f"{cnt} -> {r}/{self.count}")
+            if r < cnt:
+                # print(f"HIT")
+                return k
+        raise Exception("Tree Counter is broken.")
+
+    def write(self, cnt=0):
+        for key, val in self.nexts.items():
+            cnt = val.write(cnt+1)
+        return cnt
+
+
+class MarkovTree:
+    def __init__(self, maxi=3):
+        self.root = Word(-1, maxi)
+        self.MAXI_CHAIN = maxi
+
+    def add_tree(self, data):
+        words = ["[START]"]
+        words.extend(self.separate(data))
+        # words[-1] = "[END]"
+        words.append("[END]")
+        self.make_chain(words)
+
+    def make_chain(self, words_list):
+        for i in range(len(words_list)):
+            self.root.add(words_list[i:])
+
+    def separate(self, data):
+        mecab = MeCab.Tagger("-d /opt/mecab/lib/mecab/dic/neologd")
+        # mecab = MeCab.Tagger()
+        mecab.parse("")
+        m = mecab.parse(data)
+        lines = m.split("\n")
+        words = []
+        for line in lines:
+            words.append((re.split('[\t,]', line)[0]))
+        for _ in range(2):
+            words.pop()  # 最後の謎の空白2こを削除
+        return words
+
+    def create(self, sentencelist):
+        for s in sentencelist:
+            # s = f.read()
+            # s = self.cut_unneccesarry(s)
+            # s = self.replace_spletters(s)
+            self.add_tree(s)
+
+    def write(self):
+        self.root.write(0)
+
+
+def get_markovsing(num=1):
+    tree = MarkovTree()
+    with open("sing.tb") as f:
+        sentencelist = f.read().split("\n")
+    tree.create(sentencelist)
+    tree.write()
+
+    results = []
+    for _ in range(num):
+        lis = []
+
+        fst = "[START]"
+        # tree.root.nexts[fst].show()
+        snd = tree.root.nexts[fst].pick()
+        lis.append(snd)
+
+        while snd != "[END]":
+            newsnd = tree.root.nexts[fst].nexts[snd].pick()
+            lis.append(newsnd)
+            fst = snd
+            snd = newsnd
+        lis.pop()
+
+        results.append("".join(lis))
+    return "\n".join(results)
+
+
+if __name__ == "__main__":
+    print(get_markovsing(20))
