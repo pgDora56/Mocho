81a10452709efca5a43c0432c5e8d45892b4132d
Kazuki F <doradora.prog@gmail.com>
2021-01-02 07:32:19 +0900

    Fixed error join_notify.py

--- join_notify.py
+++ join_notify.py
@@ -1,22 +1,28 @@
 import asyncio
 
+
 class JoinNotify:
     def __init__(self, watch_category, notify_ch):
         self.watch_category = watch_category
         self.notify_ch = notify_ch
-    
+
     async def check(self, member, befch, aftch):
         msg = None
-        if befch == None:
+        if befch is None:
             if aftch.category_id == self.watch_category:
-                msg = await self.notify_ch.send(f"{member.display_name}が `{aftch}` に入室したよ(o・∇・o)")
-        elif aftch == None:
+                msg = await self.notify_ch.send(
+                    f"{member.display_name}が `{aftch}` に入室したよ(o・∇・o)")
+        elif aftch is None:
             if befch.category_id == self.watch_category:
-                msg = await self.notify_ch.send(f"{member.display_name}が `{befch}` から退出したよ(o・∇・o)")
+                msg = await self.notify_ch.send(
+                    f"{member.display_name}が `{befch}` から退出したよ(o・∇・o)")
         elif befch != aftch:
-            if before.channel.category_id == self.watch_category or after.channel.category_id == self.watch_category:
-                msg = await self.notify_ch.send(f"{member.display_name}が `{befch}` から `{aftch}` に移動したよ(o・∇・o)")
-        if msg == None:
+            if befch.category_id == self.watch_category or \
+                    aftch.category_id == self.watch_category:
+                msg = await self.notify_ch.send(
+                    f"{member.display_name}が "
+                    "`{befch}` から `{aftch}` に移動したよ(o・∇・o)")
+        if msg is None:
             return
 
         await asyncio.sleep(180)
